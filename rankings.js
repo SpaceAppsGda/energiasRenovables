a2c0bf0bcb1e5c7bf38300849449de0b037f398b
mauriciozaragoza <kingk912@gmail.com>
2013-04-20 19:48:15 -0500

    added json loading

---
+++ rankings.js
@@ -0,0 +1,9 @@
+{
+	"values": [
+		{"name": "Texas", "geo": 0.5, "solar": 0.7, "wind": 0.45}, 
+		{"name": "California", "geo": 0.6, "solar": 0.6, "wind": 0.35}, 
+		{"name": "Alabama", "geo": 0.7, "solar": 0.5, "wind": 0.75}, 
+		{"name": "Florida", "geo": 0.8, "solar": 0.4, "wind": 0.55}, 
+		{"name": "Nevada", "geo": 0.9, "solar": 0.3, "wind": 0.25}
+	]
+}
