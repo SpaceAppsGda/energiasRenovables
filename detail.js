8bd72d9456ecf4582894c6b2b09219e7ef2a286b
Antoniohdez <antonio.hdez93@gmail.com>
2013-04-21 09:47:38 -0500

    Version estable 3.2

---
+++ detail.js
@@ -0,0 +1,6 @@
+{
+	"name": "TX",
+	"solar": {"capacity1":20, "capacity2": 34},
+	"wind": {"capacity1":15, "capacity2": 23},
+	"geo": {"capacity1":7, "capacity2": 42}
+}
---
+++ rankings.js
@@ -0,0 +1,9 @@
+{
+	"values": [
+		{"name": "TX", "geo": 0.5, "solar": 0.7, "wind": 0.45}, 
+		{"name": "CA", "geo": 0.6, "solar": 0.6, "wind": 0.35}, 
+		{"name": "AL", "geo": 0.7, "solar": 0.5, "wind": 0.75}, 
+		{"name": "FL", "geo": 0.8, "solar": 0.4, "wind": 0.55}, 
+		{"name": "NV", "geo": 0.9, "solar": 0.3, "wind": 0.25}
+	]
+}
